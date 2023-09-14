e272f40a0a4bd98188f45a6633d597fa81211240
slicar2640 <101480013+slicar2640@users.noreply.github.com>
2023-09-13 21:05:40 -0500

    Add implicit multiplication

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ const equation = readline.createInterface({
   output: process.stdout,
 });
 
-equation.question("Expression to evaluate: ", function(answer) {console.log(evaluate(answer))});
+equation.question("Expression to evaluate: ", function (answer) { console.log(evaluate(answer)) });
 
 function evaluate(s) {
   let tokens = tokenize(s)
@@ -15,45 +15,47 @@ function evaluate(s) {
 }
 
 function tokenize(s) {
-  let tokens = []
+  let tokens = [];
   for (let i = 0; i < s.length; i++) {
     let char = s[i]
 
-    if (/[0-9.]/.test(char)) {
+    if (isNumber(char)) {
       let j = i
-      while (j < s.length && /[0-9.]/.test(s[j])) {
+      while (j < s.length && isNumber(s[j])) {
         j++;
       }
 
       tokens.push({
         type: "number",
         lexeme: s.substring(i, j)
-      })
+      });
 
       i = j - 1
-    } else if (/[+\-*/%^()]/.test(char)) {
+    } else if (isOperator(char)) {
       tokens.push({
         type: "operator",
         lexeme: char
-      })
+      });
     }
   }
 
-  return tokens
-}
+  for (let i = 0; i < tokens.length - 1; i++) {
+    if (tokens[i].type == "number") {
+      if (tokens[i + 1].lexeme == "(") {
+        tokens.splice(i + 1, 0, {
+          type: "operator",
+          lexeme: "*"
+        });
+      }
+    }
+  }
 
-const precedence = {
-  "^": 3,
-  "*": 2,
-  "/": 2,
-  "%": 2,
-  "+": 1,
-  "-": 1
+  return tokens;
 }
 
 function parse(tokens) {
-  let outputQueue = []
-  let operatorStack = []
+  let outputQueue = [];
+  let operatorStack = [];
 
   const precedence = {
     '+': 1,
@@ -62,42 +64,42 @@ function parse(tokens) {
     '/': 2,
     '%': 2,
     '^': 3,
-  }
+  };
 
   for (let token of tokens) {
     if (token.type == "number") {
-      outputQueue.push(token)
+      outputQueue.push(token);
     } else if (token.type == "operator") {
       if (token.lexeme === '(') {
-        operatorStack.push(token)
+        operatorStack.push(token);
       } else if (token.lexeme === ')') {
         while (operatorStack.length > 0 && operatorStack[operatorStack.length - 1].lexeme !== '(') {
-          outputQueue.push(operatorStack.pop())
+          outputQueue.push(operatorStack.pop());
         }
-        operatorStack.pop()
+        operatorStack.pop();
       } else {
         while (operatorStack.length > 0 && precedence[operatorStack[operatorStack.length - 1].lexeme] >= precedence[token.lexeme]) {
-          outputQueue.push(operatorStack.pop())
+          outputQueue.push(operatorStack.pop());
         }
-        operatorStack.push(token)
+        operatorStack.push(token);
       }
     }
   }
   while (operatorStack.length > 0) {
-    outputQueue.push(operatorStack.pop())
+    outputQueue.push(operatorStack.pop());
   }
 
-  return outputQueue
+  return outputQueue;
 }
 
 function evaluatePostfix(postfix) {
-  let stack = []
+  let stack = [];
   for (let token of postfix) {
     if (token.type == "number") {
-      stack.push(parseFloat(token.lexeme))
+      stack.push(parseFloat(token.lexeme));
     } else if (token.type == "operator") {
-      let operand2 = stack.pop()
-      let operand1 = stack.pop()
+      let operand2 = stack.pop();
+      let operand1 = stack.pop();
 
       let result;
       switch (token.lexeme) {
@@ -113,4 +115,12 @@ function evaluatePostfix(postfix) {
     }
   }
   return stack[0]
+}
+
+function isNumber(char) {
+  return /[0-9.]/.test(char)
+}
+
+function isOperator(char) {
+  return /[+\-*/%^()]/.test(char);
 }
